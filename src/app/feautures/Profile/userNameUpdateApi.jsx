70c71853b10f1b131cebbf23146ef836e26a8323
Mushahidjutt <mushahidbuttar352@gmail.com>
2025-09-05 11:58:15 +0500

    Add Profile page

--- src/app/feautures/Profile/userNameUpdateApi.jsx
+++
@@ -1,9 +0,0 @@
-import { makeApiCall } from "../../api";
-
-export const userNameUpdateApi = (data) => {
-  return makeApiCall({
-    url: "v1/users/updateMe",
-    method: "PATCH",
-    data,
-  });
-};
