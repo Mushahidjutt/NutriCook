80d3c526f7ba539d5337da32866cf71a915501f7
Mushahidjutt <mushahidbuttar352@gmail.com>
2025-09-06 23:58:57 +0500

    Add PasswordChange

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -20,15 +20,15 @@ function Home() {
     }
   };
 
-  // const handleLikedToggleRecipes = async (id) => {
-  //   try {
-  //     await likeToggleRecipeApi(id);
-  //     toast.success("Recipe Liked! 👍");
-  //     handleGetAllRecipes();
-  //   } catch (error) {
-  //     console.error("Error in Like Toggle:", error);
-  //   }
-  // };
+  const handleLikedToggleRecipes = async (id) => {
+    try {
+      await likeToggleRecipeApi(id);
+      toast.success("Recipe Liked! 👍");
+      handleGetAllRecipes();
+    } catch (error) {
+      console.error("Error in Like Toggle:", error);
+    }
+  };
 
   return (
     <div>
