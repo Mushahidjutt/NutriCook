0bbde9cc00c2091346c19b813796d665f4aa8a86
Mushahidjutt <mushahidbuttar352@gmail.com>
2025-09-25 11:00:52 +0500

    Input Update

--- src/Components/Authentication/Signup.jsx
+++ src/Components/Authentication/Signup.jsx
@@ -75,23 +75,7 @@ function Signup() {
         </p>
 
         <form onSubmit={formik.handleSubmit} className="space-y-5 my-6">
-          <div>
-            <CustomInput
-              label="Full Name"
-              type="text"
-              name="name"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.name}
-              className="w-full border border-gray-300 rounded-xl px-4 py-2 bg-white text-gray-700 focus:outline-none focus:border-pink-400 focus:ring-2 focus:ring-pink-300 shadow-sm"
-              placeholder="Enter your full name"
-            />
-            {formik.errors.name && formik.touched.name && (
-              <div className="text-sm text-red-500 ml-2">
-                {formik.errors.name}
-              </div>
-            )}
-          </div>
+          
 
           <div>
             <CustomInput
