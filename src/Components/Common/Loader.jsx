0aac8791bff2346cd6c7cb7e5281e1f4a47eab29
Mushahidjutt <mushahidbuttar352@gmail.com>
2025-10-01 11:54:41 +0500

    fix  Loader Issue

--- src/Components/Common/Loader.jsx
+++ src/Components/Common/Loader.jsx
@@ -10,7 +10,9 @@ export default function Loader({
       className={`flex items-center justify-center min-h-screen bg-white ${size} ${className}`}
     >
       <div className="flex flex-col items-center gap-4">
-       
+        <div
+          className={`animate-spin rounded-full border-4 w-16 h-16 border-t-transparent ${color}`}
+        ></div>
 
         <p className="text-lg font-semibold text-gray-700">Loading ...</p>
       </div>
